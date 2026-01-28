3da557ff44d2298318fbba0188874589ec72665a
Orbita events <ctreball20@gmail.com>
2026-01-28 22:52:55 +0100

    Update portfolio assets and add sharp

--- app/config/portfolio-images.ts
+++ app/config/portfolio-images.ts
@@ -14,7 +14,7 @@ export const PORTFOLIO_CATEGORIES = [
   {
     "slug": "eventos-empresa",
     "name": "Eventos empresa",
-    "cover": "/img/portfolio/eventos-empresa/eventos-empresa-01.webp"
+    "cover": "/img/portfolio/eventos-empresa/eventos-empresa-01.jpg"
   },
   {
     "slug": "fiestas-infantiles",
@@ -71,6 +71,18 @@ export const PORTFOLIO_IMAGES = {
     {
       "src": "/img/portfolio/bodas/bodas-01.webp",
       "alt": "Bodas – Bodas  01"
+    },
+    {
+      "src": "/img/portfolio/bodas/bodas-02.webp",
+      "alt": "Bodas – Bodas  02"
+    },
+    {
+      "src": "/img/portfolio/bodas/bodas-03.webp",
+      "alt": "Bodas – Bodas  03"
+    },
+    {
+      "src": "/img/portfolio/bodas/bodas-04.webp",
+      "alt": "Bodas – Bodas  04"
     }
   ],
   "discomovil": [
@@ -89,12 +101,108 @@ export const PORTFOLIO_IMAGES = {
     {
       "src": "/img/portfolio/discomovil/discomovil-04.webp",
       "alt": "Discomovil – Discomovil  04"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-05.webp",
+      "alt": "Discomovil – Discomovil  05"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-06.jpg",
+      "alt": "Discomovil – Discomovil  06"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-07.jpg",
+      "alt": "Discomovil – Discomovil  07"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-08.jpg",
+      "alt": "Discomovil – Discomovil  08"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-09.jpg",
+      "alt": "Discomovil – Discomovil  09"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-10.webp",
+      "alt": "Discomovil – Discomovil  10"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-11.jpg",
+      "alt": "Discomovil – Discomovil  11"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-12.jpg",
+      "alt": "Discomovil – Discomovil  12"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-13.webp",
+      "alt": "Discomovil – Discomovil  13"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-14.webp",
+      "alt": "Discomovil – Discomovil  14"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-15.jpg",
+      "alt": "Discomovil – Discomovil  15"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-16.jpg",
+      "alt": "Discomovil – Discomovil  16"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-17.jpg",
+      "alt": "Discomovil – Discomovil  17"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-18.jpg",
+      "alt": "Discomovil – Discomovil  18"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-19.jpg",
+      "alt": "Discomovil – Discomovil  19"
+    },
+    {
+      "src": "/img/portfolio/discomovil/discomovil-20.jpg",
+      "alt": "Discomovil – Discomovil  20"
     }
   ],
   "eventos-empresa": [
     {
-      "src": "/img/portfolio/eventos-empresa/eventos-empresa-01.webp",
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-01.jpg",
       "alt": "Eventos empresa – Eventos empresa  01"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-02.jpg",
+      "alt": "Eventos empresa – Eventos empresa  02"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-03.jpg",
+      "alt": "Eventos empresa – Eventos empresa  03"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-04.jpg",
+      "alt": "Eventos empresa – Eventos empresa  04"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-05.jpg",
+      "alt": "Eventos empresa – Eventos empresa  05"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-06.jpg",
+      "alt": "Eventos empresa – Eventos empresa  06"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-07.jpg",
+      "alt": "Eventos empresa – Eventos empresa  07"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-08.jpg",
+      "alt": "Eventos empresa – Eventos empresa  08"
+    },
+    {
+      "src": "/img/portfolio/eventos-empresa/eventos-empresa-09.jpg",
+      "alt": "Eventos empresa – Eventos empresa  09"
     }
   ],
   "fiestas-infantiles": [
@@ -109,6 +217,14 @@ export const PORTFOLIO_IMAGES = {
     {
       "src": "/img/portfolio/fiestas-infantiles/fiestas-infantiles-03.webp",
       "alt": "Fiestas infantiles – Fiestas infantiles  03"
+    },
+    {
+      "src": "/img/portfolio/fiestas-infantiles/fiestas-infantiles-04.jpg",
+      "alt": "Fiestas infantiles – Fiestas infantiles  04"
+    },
+    {
+      "src": "/img/portfolio/fiestas-infantiles/fiestas-infantiles-05.jpg",
+      "alt": "Fiestas infantiles – Fiestas infantiles  05"
     }
   ],
   "fiestas-privadas": [
@@ -119,12 +235,164 @@ export const PORTFOLIO_IMAGES = {
     {
       "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-02.webp",
       "alt": "Fiestas privadas – Fiestas privadas  02"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-03.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  03"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-04.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  04"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-05.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  05"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-06.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  06"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-07.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  07"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-08.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  08"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-09.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  09"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-10.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  10"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-11.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  11"
+    },
+    {
+      "src": "/img/portfolio/fiestas-privadas/fiestas-privadas-12.jpg",
+      "alt": "Fiestas privadas – Fiestas privadas  12"
     }
   ],
   "fiestas-tematicas-halloween": [
     {
       "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-01.webp",
       "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  01"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-02.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  02"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-03.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  03"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-04.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  04"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-05.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  05"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-06.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  06"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-07.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  07"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-08.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  08"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-09.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  09"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-10.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  10"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-11.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  11"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-12.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  12"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-13.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  13"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-14.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  14"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-15.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  15"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-16.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  16"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-17.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  17"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-18.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  18"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-19.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  19"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-20.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  20"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-21.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  21"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-22.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  22"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-23.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  23"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-24.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  24"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-25.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  25"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-26.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  26"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-27.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  27"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-28.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  28"
+    },
+    {
+      "src": "/img/portfolio/fiestas-tematicas-halloween/fiestas-tematicas-halloween-29.jpg",
+      "alt": "Fiestas tematicas halloween – Fiestas tematicas halloween  29"
     }
   ],
   "fiestas-tematicas-mon-magic": [
