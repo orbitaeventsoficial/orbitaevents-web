2bd064bf9f24dadb6cac458eb94e1a4491b90bdd
Orbita events <ctreball20@gmail.com>
2026-02-22 03:00:59 +0100

    admin: isolate solid theme tokens in dedicated admin stylesheet

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import dynamicImport from 'next/dynamic';
 import { usePathname, useRouter } from 'next/navigation';
 import { AdminHelpModeProvider, useAdminHelpMode } from './components/AdminHelpMode';
+import './admin-theme.css';
 
 const AdminSearchModal = dynamicImport(() => import('./components/AdminSearchModal'), {
   ssr: false,
